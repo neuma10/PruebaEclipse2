2871ce7d8306306546c1d4b601e62304b6651f74
Galleguin_mzz <sg9787188@gmail.com>
2023-05-17 12:56:21 +0200

    Añado cambios a prueba

--- src/Prueba.java
+++ src/Prueba.java
@@ -2,5 +2,6 @@
 public class Prueba {
 	public static void main (String [] args) {
 		System.out.println("neumaticos el del moral");
+		System.out.println("MAMAHUEVO CHUPAPINGAS HIJO DE LA GRAN Y BONITA MADRE QUE TIENES <3");
 	}
 }
